2b12c5e4205edcfc3c7c74ccc6a4b499637dab76
Milena Felix <milena.ads2018@gmail.com>
2026-07-28 11:04:34 -0300

    feat: criação do init.sql

--- init.sql
+++ init.sql
@@ -1,12 +1,12 @@
-CREATE TABLE Parcerias (
+CREATE TABLE IF NOT EXISTS Parcerias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome_empresa VARCHAR(100) NOT NULL,
     nome_programa VARCHAR(100),
-    status ENUM('Ativo', 'Inativo') DEFAULT 'Ativo',
+    status ENUM('Ativo', 'Inativo', 'Pendente', 'Renovado') DEFAULT 'Ativo',
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Pautas (
+CREATE TABLE IF NOT EXISTS Pautas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     parceria_id INT,
     titulo VARCHAR(200) NOT NULL,
@@ -16,5 +16,5 @@ CREATE TABLE Pautas (
 );
 
 -- Inserindo um dado de teste real para contexto
-INSERT INTO Parcerias (nome_empresa, nome_programa) 
-VALUES ('Editora Valentina', 'Amigos da Valen');
+INSERT INTO Parcerias (nome_empresa, nome_programa, status) 
+VALUES ('Editora Valentina', 'Amigos da Valen', 'Ativo');
